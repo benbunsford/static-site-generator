283cd385b38c1b88e078627613c2820c8d631f95
benbunsford <benbridgford@gmail.com>
2025-11-03 18:32:54 -0800

    refactored markdown to html node function

--- src/markdown_to_html_node.py
+++ src/markdown_to_html_node.py
@@ -1,6 +1,7 @@
 import re
 
 from block_to_block_type import block_to_block_type, BlockType
+from leafnode import LeafNode
 from markdown_to_blocks import markdown_to_blocks
 from text_node_to_leaf_node import text_node_to_leaf_node
 from text_to_textnodes import text_to_textnodes
@@ -14,93 +15,81 @@ def markdown_to_html_node(markdown_doc):
     blocks_as_html_nodes = []
     for block in blocks:
         block_type = block_to_block_type(block)
-        block_html_node = block_to_html_node(block, block_type)
-        cleaned_block_text = clean_text(block, block_type)
-        blocks_as_html_nodes.append(block_html_node)
-        if block_type == BlockType.CODE:
-            code_node = TextNode(cleaned_block_text, TextType.CODE)
-            code_leaf_node = text_node_to_leaf_node(code_node)
-            block_html_node.children.append(code_leaf_node)
-        else:
-            stripped_lines = strip_lines(cleaned_block_text)
-            inline_nodes = text_to_children(stripped_lines)
-            block_html_node.children.extend(inline_nodes)
+        tag = get_tag(block, block_type)
+        stripped_block = strip_block(block, block_type)
+        block_node = block_to_nodes(stripped_block, block_type, tag)
+        blocks_as_html_nodes.append(block_node)
     div = ParentNode("div", blocks_as_html_nodes)
     return div
 
-def clean_text(block, block_type):
+def get_tag(block, block_type):
     match block_type:
         case BlockType.PARAGRAPH:
-            return block
+            return "p"
+        case BlockType.HEADING:
+            num_hashes = len(block) - len(block.lstrip('#'))
+            return f"h{num_hashes}"
+        case BlockType.CODE:
+            return "pre"
+        case BlockType.QUOTE:
+            return "blockquote"
+        case BlockType.UNORDERED_LIST:
+            return "ul"
+        case BlockType.ORDERED_LIST:
+            return "ol"
+        case _:
+            raise TypeError("error: not a valid BlockType")
+
+def strip_block(block, block_type):
+    block = block.strip()
+    match block_type:
+        case BlockType.PARAGRAPH:
+            return strip_paragraph(block)
         case BlockType.HEADING:
             return block.lstrip('# ')
         case BlockType.CODE:
-            stripped = block.strip("```")
+            stripped = block[4:-3]
             if stripped.startswith("\n"):
                 stripped = stripped[1:]
             return stripped
         case BlockType.QUOTE:
-            return block.strip("> ")
-        case BlockType.UNORDERED_LIST:
-            list_items = []
-            lines = block.split("\n")
-            for line in lines:
-                m = re.match(r"^\s*[-*+]\s+(.*)$", line)
-                if m:
-                    stripped = m.group(1)
-                    list_items.append(stripped)
-            return "\n".join(list_items)
-        case BlockType.ORDERED_LIST:
-            list_items = []
-            lines = block.split("\n")
-            for line in lines:
-                m = re.match(r"^\s*(\d+)\.\s+(.*)$", line)
-                if m:
-                    stripped = m.group(2)
-                    list_items.append(stripped)
-            return "\n".join(list_items)
+            return strip_quote(block)
         case _:
-            raise TypeError("error: not a valid BlockType")
+            return block
 
-def strip_lines(text):
-    lines = text.split("\n")
-    stripped_list = []
+def strip_list_items(block, block_type):
+    list_items = []
+    lines = block.split("\n")
+    for l in lines:
+        line = l.strip()
+        match = None
+        if block_type == BlockType.UNORDERED_LIST:
+            match = re.match(r"^- (.*)", line)
+        elif block_type == BlockType.ORDERED_LIST:
+            match = re.match(r"^\d\. (.*)", line)
+        if match:
+            stripped = match.group(1)
+            list_items.append(stripped)
+    return list_items
+
+def strip_quote(block):
+    line_list = []
+    lines = block.split("\n")
     for line in lines:
-        stripped = line.strip().lstrip("- >")
-        if stripped:
-            stripped_list.append(stripped)
-    return " ".join(stripped_list)
+        match = re.match(r"^> (.*)", line)
+        if match:
+            stripped = match.group(1)
+            line_list.append(stripped)
+    return " ".join(line_list)
 
-def block_to_html_node(block, block_type):
-    match block_type:
-        case BlockType.PARAGRAPH:
-            return ParentNode("p", [])
-        case BlockType.HEADING:
-            num_hashes = len(block) - len(block.lstrip('#'))
-            return ParentNode(f"h{num_hashes}", [])
-        case BlockType.CODE:
-            pre_node = ParentNode("pre", [])
-            return pre_node
-        case BlockType.QUOTE:
-            return ParentNode("blockquote", [])
-        case BlockType.UNORDERED_LIST:
-            list_items = []
-            lines = block.split("\n")
-            for line in lines:
-                stripped_line = strip_lines(line)
-                line_nodes = text_to_children(stripped_line)
-                list_items.append(ParentNode("li", line_nodes))
-            return ParentNode("ul", list_items)
-        case BlockType.ORDERED_LIST:
-            list_items = []
-            lines = block.split("\n")
-            for line in lines:
-                stripped_line = strip_lines(line)
-                line_nodes = text_to_children(stripped_line)
-                list_items.append(ParentNode("li", line_nodes))
-            return ParentNode("ol", list_items)
-        case _:
-            raise TypeError("error: not a valid BlockType")
+def strip_paragraph(block):
+    line_list = []
+    lines = block.split("\n")
+    for line in lines:
+        stripped = line.strip()
+        if stripped:
+            line_list.append(stripped)
+    return " ".join(line_list)
 
 def text_to_children(text):
     inline_text_nodes = text_to_textnodes(text)
@@ -108,4 +97,18 @@ def text_to_children(text):
     for node in inline_text_nodes:
         inline_html_nodes.append(text_node_to_leaf_node(node))
     return inline_html_nodes
+
+def block_to_nodes(block, block_type, tag):
+    if block_type == BlockType.UNORDERED_LIST or block_type == BlockType.ORDERED_LIST:
+        line_nodes = []
+        stripped_lines = strip_list_items(block, block_type)
+        for line in stripped_lines:
+            line_nodes.append(ParentNode("li", text_to_children(line)))
+        return ParentNode(tag, line_nodes)
+    elif block_type == BlockType.CODE:
+        code_node = TextNode(block, TextType.CODE)
+        code_leaf_node = text_node_to_leaf_node(code_node)
+        return ParentNode(tag, [code_leaf_node])
+    else:
+        return ParentNode(tag, text_to_children(block))
 
